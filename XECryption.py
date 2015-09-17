0a65f72a3ba55437f95a5f984ee9a8a69b1a749b
Rudolf Copi Eckelberg <rudolfce@gmail.com>
2015-09-17 13:05:38 +0000

    File handling, comments syntax and var naming

--- XECryption.py
+++ XECryption.py
@@ -1,20 +1,30 @@
+#!/usr/bin/env python
+
 import sys
 
-in_file = open(sys.argv[1], 'r')
+if len(sys.argv) != 2:
+    print "Usage: {} [encrypted file].".format(sys.argv[0])
+    sys.exit(1)
+
+try:
+    in_file = open(sys.argv[1], 'r')
+except IOError:
+    sys.stderr.write("Could not open file {}\n".format(sys.argv[1]))
+    sys.exit(1)
 
 in_stream = [int(n) for n in in_file.read().split('.') if n]
-''' This will separate the numbers after each dot and ignore empty
-    numbers, but won't sanitize the string in any other way.'''
+# This will separate the numbers after each dot and ignore empty
+# numbers, but won't sanitize the string in any other way.
 
 i = 0
-n_sum = 0
+encrypted_char = 0
 raw_outstream = []
 for n in in_stream: # Find the encoded number for each character of the message
-    n_sum += n
-    if i%3 == 2:
-        raw_outstream.append(n_sum)
-        n_sum = 0
+    encrypted_char += n
     i += 1
+    if not i%3:
+        raw_outstream.append(encrypted_char)
+        encrypted_char = 0
 
 # First subtraction factor - min value for an ASCII char not lower than 0
 key = min(raw_outstream)
@@ -24,3 +34,5 @@ while not in_char == 'q':
     print ''.join(out_stream)
     in_char = raw_input()
     key -= 1
+
+sys.exit(0)
